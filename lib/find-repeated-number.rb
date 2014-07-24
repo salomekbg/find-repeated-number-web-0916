ffabc4bde0ff332b2e3ff7c88186b630fe0d2dcc
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-24 16:55:14 -0400

    solution branch

--- lib/find-repeated-number.rb
+++ lib/find-repeated-number.rb
@@ -1,3 +1,10 @@
 def find_repeated_number(array)
-
+  number_check = {}
+  array.each do |number|
+    if number_check[number] == "seen"
+      return number
+    else
+      number_check[number] = "seen"
+    end
+  end
 end
